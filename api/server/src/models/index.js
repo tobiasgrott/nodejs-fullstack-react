46cbe99259c8fabb18587a1ce47990c487ec7340
tobiasgrott <tobiasgrott@gmail.com>
2019-11-11 14:21:45 +0100

    transformed to ES6

--- api/server/src/models/index.js
+++ api/server/src/models/index.js
@@ -1,32 +1,55 @@
-'use strict';
+import fs from 'fs';
+import path from 'path';
+import Sequelize from 'sequelize';
+import configJson from '../config/config';
 
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.js')[env];
+const env = process.env.NODE_ENV ? process.env.NODE_ENV : 'development';
+
+const config = configJson[env];
+
+console.log('this is the environment: ', env);
+
 const db = {};
 
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+if (config.environment === 'production'){
+  sequelize = new Sequelize(
+    process.env[config.use_env_variable], config
+    );
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASS, {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      dialect: 'postgres',
+      dialectOption: {
+        ssl: true, 
+        native: true
+      },
+      logging: true
+    }
+  );
+}else{
+  sequelize = new Sequelize(
+    config.database, config.username,config.password, config
+  );
 }
 
 fs
   .readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+  .filter((file) => {
+    return (file.indexOf('.') !== 0) &&
+           (file !== basename) && (file.slice(-3) = '.js');
   })
-  .forEach(file => {
-    const model = sequelize['import'](path.join(__dirname, file));
+  .forEach((file) => {
+    const model = sequelize.import(path.join(__dirname, file));
     db[model.name] = model;
   });
 
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
+Object.keys(db).forEach((modelName) => {
+  if(db[modelName].associate){
     db[modelName].associate(db);
   }
 });
@@ -34,4 +57,4 @@ Object.keys(db).forEach(modelName => {
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
-module.exports = db;
+export default db;
